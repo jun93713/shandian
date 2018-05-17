d6d493c2b22a1d302eba4b32ede9cdfb440fdcf8
jun93713 <jun93713@hotmail.com>
2018-05-16 20:33:24 -0700

    final modify

--- shandian/settings.py
+++ shandian/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h&-as4#**4j_7+g^fr5u6a99iz=jem1ax=)=^d!w=sdge!x0#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
